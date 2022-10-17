32f150009a006c39bc69c29fbcd9fd8833838508
FUZAIL AHAMED. S <15699A0419@mits.ac.in>
2022-10-17 06:31:25 +0000

    Added main.tf

---
+++ ArmTemplates/main.tf
@@ -0,0 +1,85 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.92.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+locals {
+  resource_group="app-grp"
+  location="North Europe"  
+}
+
+
+resource "azurerm_resource_group" "app_grp"{
+  name=local.resource_group
+  location=local.location
+}
+
+resource "azurerm_app_service_plan" "app_plan1000" {
+  name                = "app-plan1000"
+  location            = azurerm_resource_group.app_grp.location
+  resource_group_name = azurerm_resource_group.app_grp.name
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "webapp" {
+  name                = "webapp5539050"
+  location            = azurerm_resource_group.app_grp.location
+  resource_group_name = azurerm_resource_group.app_grp.name
+  app_service_plan_id = azurerm_app_service_plan.app_plan1000.id
+     
+  depends_on=[azurerm_app_service_plan.app_plan1000]
+}
+
+resource "azurerm_sql_server" "app_server_6008089" {
+  name                         = "appserver6008089"
+  resource_group_name          = azurerm_resource_group.app_grp.name
+  location                     = "North Europe"  
+  version             = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "Azure123"
+}
+
+resource "azurerm_sql_database" "app_db" {
+  name                = "appdb"
+  resource_group_name = azurerm_resource_group.app_grp.name
+  location            = "North Europe"  
+  server_name         = azurerm_sql_server.app_server_6008089.name
+   depends_on = [
+     azurerm_sql_server.app_server_6008089
+   ]
+}
+
+resource "azurerm_sql_firewall_rule" "app_server_firewall_rule_Azure_services" {
+  name                = "app-server-firewall-rule-Allow-Azure-services"
+  resource_group_name = azurerm_resource_group.app_grp.name
+  server_name         = azurerm_sql_server.app_server_6008089.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+  depends_on=[
+    azurerm_sql_server.app_server_6008089
+  ]
+}
+
+resource "azurerm_sql_firewall_rule" "app_server_firewall_rule_Client_IP" {
+  name                = "app-server-firewall-rule-Allow-Client-IP"
+  resource_group_name = azurerm_resource_group.app_grp.name
+  server_name         = azurerm_sql_server.app_server_6008089.name
+  start_ip_address    = "4.246.169.117"
+  end_ip_address      = "4.246.169.117"
+  depends_on=[
+    azurerm_sql_server.app_server_6008089
+  ]
+}
