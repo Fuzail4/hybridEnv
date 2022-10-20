4bd438268add5f9ebdd0f5b74de769823790e104
FUZAIL AHAMED. S <15699A0419@mits.ac.in>
2022-10-20 03:21:28 +0000

    Updated main.tf

--- ArmTemplates/main.tf
+++ ArmTemplates/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "2.92.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 
 locals {
   resource_group="app-grp"
