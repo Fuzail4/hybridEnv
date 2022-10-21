fe3c3e885ada2625d2ef541cab976c8ecc79dd4e
Fuzail4 <116172822+Fuzail4@users.noreply.github.com>
2022-10-21 11:06:26 +0530

    Update main.tf

--- Templates/main.tf
+++ Templates/main.tf
@@ -42,7 +42,7 @@ resource "azurerm_app_service" "webapp" {
   name                = local.web_app_name
   location            = azurerm_resource_group.app_grp.location
   resource_group_name = azurerm_resource_group.app_grp.name
-  app_service_plan_id = azurerm_app_service_plan.app_plan1000.id
+  app_service_plan_id = azurerm_app_service_plan.app_plan.id
      
   depends_on=[azurerm_app_service_plan.app_plan]
 }
